1edd879b3f787b759a71b852859bae73e5ac2a6d
davinellulinvega <davinellulinvega@nyaldee.com>
2016-04-22 18:43:53 +0100

    Debug the pulseaudio class

--- Pulseaudio.py
+++ Pulseaudio.py
@@ -14,18 +14,18 @@ class Pulseaudio():
         """
         self._pulse = Pulse(threading_lock=True)
 
-    def get_sink(self, app_name):
+    def get_sinks(self, app_name):
         """
         Get the sink corresponding to the given application
         :param app_name: Name of the application
-        :return: None if no corresponding sink is found or sink is mute, PA_SINK otherwise
+        :return: None if no corresponding sink is found, PA_SINK object otherwise
         """
 
         # Initialize the sink
-        app_sink = None
+        app_sink = []
 
         # Make sure the app_name is a string
-        if app_name is not isinstance(app_name, str) and app_name is not None:
+        if not isinstance(app_name, str) and app_name is not None:
             raise TypeError("Application name should be a String")
 
         # Get the list of input sinks
@@ -34,11 +34,14 @@ class Pulseaudio():
             # Get the name of the binary associated with the sink
             sink_app_name = sink.proplist["application.process.binary"]
             # Check that the sink is not muted and correspond to the app_name given in parameter
-            if sink.mute == 0 and sink_app_name == app_name.lower():
-                app_sink = sink
+            if sink_app_name == app_name.lower():
+                app_sink.append(sink)
 
         # Return the sink object
-        return app_sink
+        if len(app_sink) != 0:
+            return app_sink
+        else:
+            return None
 
     def short_press(self, app_name=None):
         """
@@ -49,13 +52,14 @@ class Pulseaudio():
 
         if app_name is not None:
             # Get the corresponding sink
-            sink = self.get_sink(app_name)
+            sinks = self.get_sinks(app_name)
             # If we got the sink
-            if sink is not None:
-                # Get the mute state
-                muted = bool(sink.mute)
-                # Toggle the state
-                self._pulse.mute(sink, mute=not muted)
+            if sinks is not None:
+                for sink in sinks:
+                    # Get the mute state
+                    muted = bool(sink.mute)
+                    # Toggle the state
+                    self._pulse.mute(sink, mute=not muted)
             else:
                 # TODO: PLAY-PAUSE CLEMENTINE
                 pass
@@ -70,8 +74,9 @@ class Pulseaudio():
 
         if app_name is not None:
             # Get the sink corresponding to the app
-            sink = self.get_sink(app_name)
-            if sink is not None:  # The sink was found and is not muted
-                self._pulse.volume_change_all_chans(sink, rotation * 0.005)
-            else:  # We'll try to change the volume corresponding to clementine media player
-                self.rotate(rotation, "clementine")
+            sinks = self.get_sinks(app_name)
+            for sink in sinks:
+                if sink.mute == 0 and sink is not None:  # The sink was found and is not muted
+                    self._pulse.volume_change_all_chans(sink, rotation * 0.005)
+                else:  # We'll try to change the volume corresponding to clementine media player
+                    self.rotate(rotation, "clementine")
